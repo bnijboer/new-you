85a03e419b7cf88075771be966e7c9333c1974db
bnijboer <brendannijboer@gmail.com>
2020-09-24 22:12:11 +0200

    made banner smaller

--- resources/views/_header.blade.php
+++ resources/views/_header.blade.php
@@ -1,7 +1,7 @@
 <div class="relative bg-blue-800 m-4">
-    <img class="h-64 w-full object-cover opacity-25" src="{{ asset('images/header.jpg') }}">
-    <div class="absolute top-0 w-full h-64 z-1">
-        <div class="text-center mt-24 xl:mt-16 text-5xl xl:text-7xl text-white">
+    <img class="h-32 w-full object-cover opacity-25" src="{{ asset('images/header.jpg') }}">
+    <div class="h-32 w-full absolute top-0 z-1">
+        <div class="text-center mt-6 text-5xl text-white">
             @yield('banner-text')
         </div>
     </div>
