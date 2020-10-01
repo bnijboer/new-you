686fb4130ef51ce6d261f062a5820fe9db54eee4
bnijboer <brendannijboer@gmail.com>
2020-10-01 21:16:08 +0200

    add 405 error page

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.error')
+
+@section('title', '405 Error')
+
+@section('content')
+
+    <div>
+        <p class="font-medium">
+            405 error
+        </p>
+        <p class="mt-3">
+            This action is not allowed.
+        </p>
+    </div>
+
+@endsection
