d85cfe611d79a1ccace92510a859ad5ccc7ef007
bnijboer <brendannijboer@gmail.com>
2020-10-08 16:50:01 +0200

    clean up tailwindcss scaffolding

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }} - @yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    
+    <title>{{ config('app.name', 'New You') }} - @yield('title')</title>
+    
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css">
+    
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     @stack('scripts')
@@ -19,7 +20,7 @@
         {{ $slot }}
     </div>
     
-    <footer class="bg-gray-200 bg-opacity-75 tracking-wide text-gray-600 text-center py-4">
+    <!-- <footer class="bg-gray-200 bg-opacity-75 tracking-wide text-gray-600 text-center py-4">
         
         Brendan Nijboer ©
         
@@ -39,5 +40,5 @@
                 <i class="fab fa-linkedin-in"></i>
             </a>
         </div>
-    </footer>
+    </footer> -->
 </body>
